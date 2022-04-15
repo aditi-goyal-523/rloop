cde9007deb663d2408ee0e3140ccfc5215a5ac30
aditigo <adigoyal@ucdavis.edu>
2022-04-15 15:22:54 -0700

    write processed df to new file, include summary stats

--- processnarrowpeak.py
+++ processnarrowpeak.py
@@ -15,19 +15,43 @@ def processnarrowpeak(df):
     df['length']=df['end']-df['start']
     df['count']=df['count']/10
     df['peak']=df['start']+df['peak']
-    df=df[['start', 'end', 'length', 'count', 'peak']]
+    df=df[['chr', 'start', 'end', 'length', 'count', 'peak']]
     return(df)
 
 def statsnarrowpeak(df):
     means=df[['length', 'count']].mean()
-    return(means)
+    m_length=means[0]
+    m_count=means[1]
 
+    sds=df[['length', 'count']].std()
+    s_length=sds[0]
+    s_count=sds[1]
+
+    summary=pd.DataFrame({
+        "length":[m_length, s_length], 
+        "count":[m_count, s_count]
+    })
+    summary.index=["mean", "st. dev"]
+    #frames = [means, sds]
+    #result = pd.concat(frames)
+    return(summary)
 
 #----------------------------------------------------
+
+
 
 df=readnarrowpeak(sys.argv[1])
 df=abridgenarrowpeak(df)
 df=processnarrowpeak(df)
+stats=statsnarrowpeak(df)
 
-print(df.to_string())	
-print(statsnarrowpeak(df))
+#print out the summary statistics
+print(stats)
+
+#this section only applicable for the narrowpeak files generated from macs2 given nomenclature
+#write out the new style file to a different file
+sections=sys.argv[1].split(".")
+df.to_csv(sections[0]+"."+sections[2]+".processed.txt", sep="\t", index=False) #includes label + pos/neg + processed.txt
+
+	
+
